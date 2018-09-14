f785f3feb2be888e925263096465b8e9ffd8ba06
rockstardotb <alex37184@yahoo.com>
2018-09-13 21:07:44 -0500

    reformatted

--- fixed_point_iteration.cpp
+++ fixed_point_iteration.cpp
@@ -31,12 +31,12 @@ public:
     // fixed point iteration
     void fpi(){
         x_i = guess;
-	double delta_x;
-	double x_o;
+        double delta_x;
+        double x_o;
         converged = false;
-	count = 0;
+        count = 0;
         for(int i=1 ; i <= max_iter ; i++){
-	    x_o = x_i;
+            x_o = x_i;
             x_i = func_g(res,res_prime,x_i,guess);
             delta_x = abs(x_o - x_i);
             count++;
@@ -47,16 +47,16 @@ public:
         }
     }
     void show(){
-	if(converged){
-		std::cout << setprecision(8);
-		std::cout << "g(x) converged in " << count << " iterations:\n";
+        if(converged){
+            std::cout << setprecision(8);
+            std::cout << "g(x) converged in " << count << " iterations:\n";
         	std::cout << "x = " << x_i;
         	std::cout << "\n";
-	}
-	else{
-		std::cout << "Function did not converge after ";
-		std::cout << count << " iterations.\n";
-	}
+        }
+        else{
+            std::cout << "Function did not converge after ";
+            std::cout << count << " iterations.\n";
+        }
     }
 };
 
@@ -119,3 +119,12 @@ int main()
     
     return 0;
 }
+/* Sample Output
+ Function did not converge after 100000 iterations.
+ 
+ g(x) converged in 9 iterations:
+ x = -0.97089892
+ 
+ g(x) converged in 8280 iterations:
+ x = 1.5921512
+ */
